a96a546af0e6ca036118beb52e7acaae9ea5d671
Chanson9892 <chan.hanson5@gmail.com>
2021-03-08 17:30:49 -0800

    can show numbers on the display

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-del]')
 const clearButton = document.querySelector('[data-clear]')
-const prevOperand = document.querySelector('[data-prev]')
-const curOperand = document.querySelector('[data-cur]')
+const prevOperandText = document.querySelector('[data-prev]')
+const curOperandText = document.querySelector('[data-cur]')
 
 
 class Calculator {
-    constructor(prevOperand, curOperand){
-        this.prevOperand = prevOperand
-        this.curOperand = curOperand
+    constructor(prevOperandText, curOperandText){
+        this.prevOperandText = prevOperandText
+        this.curOperandText = curOperandText
         this.clear()
     }
 
@@ -26,13 +26,13 @@ class Calculator {
     }
 
     showNumber(number) {
-        if (number === '.' && this.curOperand.includes('.')) return;
+        if (number === '.' && this.curOperand.includes('.')) return
         this.curOperand = this.curOperand.toString() + number.toString()
 
     }
 
     chooseOperation(operation) {
-
+        
     }
 
     compute() {
@@ -40,13 +40,20 @@ class Calculator {
     }
 
     updateDisplay() {
-        this.curOperand.innerText = this.curOperand
+        this.curOperandText.innerText = this.curOperand
     }
 }
 
-const calculator = new Calculator(prevOperand, curOperand)
+const calculator = new Calculator(prevOperandText, curOperandText)
 
 numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.showNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
